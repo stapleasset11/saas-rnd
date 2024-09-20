ccd778a2627975fb3f03bc0de554d11c676f5fd4
stapleasset11 <mwavulalenjo@gmail.com>
2024-09-20 13:36:28 +0300

    updated views to change order

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -9,15 +9,16 @@ def home_page_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
     my_title = "My Page"
+    path = request.path
+    print('path',path)
+    html_template = "home.html"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
         'total_visit_count':qs.count(),
         'Conversion_percentage': page_qs.count() * 100.0 / qs.count()
     }
-    path = request.path
-    print('path',path)
-    html_template = "home.html"
+    
     PageVisit.objects.create(path=request.path)
     return render(request, html_template,my_context)
 

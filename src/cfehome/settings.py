c3441720277618da6df654e4154e7a0be6a47af1
stapleasset11 <mwavulalenjo@gmail.com>
2024-09-27 05:49:31 +0300

    configuring whitenoise

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -159,8 +160,14 @@ STATIC_FILES_DIRS = [
 #Output for python manage.py collectstatic
 # local cdn
 STATIC_ROOT = BASE_DIR / 'local-cdn'
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- src/commando/management/commands/vendor_pull.py
+++ src/commando/management/commands/vendor_pull.py
@@ -11,6 +11,8 @@ VENDOR_STATICFILES = {
     "flowbite.min.css": "https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css",
 
     "flowbite.min.js": "https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js",
+
+    "flowbite.min.js.map": "https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js",
 }
 
 class Command(BaseCommand):
